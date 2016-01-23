33c1c34c0ea79b9a2ca0af46f5dbafe0d12c066d
eharbers <erik.harbers64@gmail.com>
2016-01-23 20:54:44 +0100

    p252

--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@ class Settings(object):
     """A class to store all settings."""
     
     def __init__(self):
-        """Initialize the gam's settings."""
-        # SCreen setings
+        """Initialize the game's settings."""
+        # Screen setings
         self.screen_width = 800
         self.screen_height = 600
         self.bg_color = (230,230,230)
